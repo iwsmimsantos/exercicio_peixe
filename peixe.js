03e1682dbabd08b9d3e487c087047a0066b58f2e
iwsmimsantos <158859354+iwsmimsantos@users.noreply.github.com>
2024-02-18 19:18:16 -0300

    Revert "criar pastas e configurar index.html"
    This reverts commit 818c601ce540f09e28c36d8cbbcdf11a3508c2f7.

--- peixe.js
+++ peixe.js
@@ -1,15 +1,9 @@
-    // conjunto de configurações básicas do nosso jogo. 
 var config = {
-
-    // Phaser.AUTO -- Escolha automática (pelo navegador) do tipo de renderizador da página web
     type: Phaser.AUTO,
-    
-    //indicam a largura e altura da tela do nosso jogo respectivamente
     width: 800, 
     height: 600,
 
     scene: {
-
         preload: preload,
         create: create,
         update: update
